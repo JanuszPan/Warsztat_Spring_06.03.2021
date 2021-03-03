4aedaf582aeab8a611b14945c1a00874eb59d750
JanuszPan <janusz.panczyk@gmail.com>
2021-03-03 16:55:22 +0100

    BookService

--- src/main/java/pl/coderslab/app/BookController.java
+++ src/main/java/pl/coderslab/app/BookController.java
@@ -1,14 +1,30 @@
 package pl.coderslab.app;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/books")
 public class BookController {
+   private  BookService bookService;
+   public BookController(BookService bookService){
+       this.bookService=bookService;
+   }
+   @GetMapping("")
+   public @ResponseBody
+    List<Book>getList(){
+       return bookService.getBooks();
+   }
+
 
     @RequestMapping("/helloBook")
     public Book helloBook() {
         return new Book(1L, "9788324631766", "Thinking in Java",
                 "Bruce Eckel", "Helion", "programming");
     }
+
 }
---
+++ src/main/java/pl/coderslab/app/MemoryBookService.java
@@ -0,0 +1,48 @@
+package pl.coderslab.app;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class MemoryBookService implements BookService {
+    private List<Book> list = new ArrayList<>();
+    private  BookService bookService;
+
+    public MemoryBookService() {
+        list.add(new Book(1L, "9788324631766", "Thiniking	in	Java", "Bruce	Eckel", "Helion", "programming"));
+        list.add(new Book(2L, "9788324627738", "Rusz	glowa	Java.", "Sierra	Kathy,	Bates	Bert", "Helion",
+                "programming"));
+        list.add(new Book(3L, "9780130819338", "Java	2.	Podstawy", "Cay	Horstmann,	Gary	Cornell", "Helion",
+                "programming"));
+    }
+
+    @Override
+    public List<Book> getBooks() {
+        return bookService.getBooks();
+    }
+
+    @Override
+    public void add(Book book) {
+
+    }
+
+    @Override
+    public Optional<Book> get(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void update(Book book) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
